b9fa45433ef758aaa08b7d8f89ef390c917274c4
Karyna Okuneva <karynaokuneva@gmail.com>
2025-11-26 18:33:15 +0100

    add new file match.js, add match`s model

---
+++ model/matches.js
@@ -0,0 +1,22 @@
+const mongoose = reqiure("mongoose");
+
+const matchModel = new mongoose.Schema({
+  date: {
+    type: string,
+    required: true,
+  },
+  teamHome: {
+    type: string,
+    reqiured: true,
+  },
+  teamAway: {
+    type: string,
+    required: true,
+  },
+  score: {
+    type: string,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Match", matchModel);
